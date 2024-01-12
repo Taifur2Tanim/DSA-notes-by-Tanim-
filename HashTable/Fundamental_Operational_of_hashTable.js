bdaa1da322d8880cba67864d98b2bd641479400a
Taifur2Tanaim <taifurmohammadt@gmail.com>
2024-01-12 23:12:00 +0600

    1st of HashTable note

---
+++ HashTable/Fundamental_Operational_of_hashTable.js
@@ -0,0 +1,38 @@
+/*
+   // 1. Initialization
+const hashTable = {};
+
+// 2. Insertion (Addition)
+hashTable["key1"] = "value1";
+hashTable["key2"] = "value2";
+
+// 3. Deletion
+delete hashTable["key1"];
+
+// 4. Search (Retrieval)
+const value = hashTable["key2"];
+
+// 5. Collision Handling
+
+// 6. Hash Function
+
+// 7. Load Factor
+
+// 8. Resizing
+
+// 9. Collision Resolution Strategies
+
+// 10. Key Uniqueness
+
+// 11. Iteration
+for (const key in hashTable) {
+  const value = hashTable[key];
+  // Process key-value pair
+}
+
+// 12. Time Complexity
+// On average, hash table operations have O(1) time complexity.
+// Worst-case scenarios can degrade to O(n) in the presence of frequent collisions.
+
+
+*/
